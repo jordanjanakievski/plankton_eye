3b0eb61bd1e8839874450f37a71a150a72f0b1c0
JSquared2924 <41130718+JSquared2924@users.noreply.github.com>
2021-01-21 13:03:10 -0800

    Upload of all current files for the app

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:object_detection/realtime/live_camera.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  // initialize the cameras when the app starts
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  // running the app
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.lightBlue,
+    ),
+    darkTheme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.lightBlue,
+    ),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.lightBlue,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Spacer(),
+              Image.asset(
+                "assets/logo.png",
+                width: 300,
+                height: 300,
+              ),
+              Text("Plankton Eye",
+                  style: GoogleFonts.openSans(
+                    textStyle: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+                  )),
+              Spacer(),
+              ButtonTheme(
+                minWidth: 300,
+                child: OutlinedButton(
+                  child: Text(
+                    "Start Detection",
+                    style: GoogleFonts.openSans(
+                    textStyle: TextStyle(color: Colors.white, fontSize: 30),
+                  )),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LiveFeed(cameras),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Spacer(flex: 2),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: IconButton(
+                  icon: Icon(Icons.check_circle, color: Colors.white),
+                  onPressed: aboutDialog,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //This includes all the developer specifics and allows the viewing of all the licenses being used
+  aboutDialog() {
+    showAboutDialog(
+      context: context,
+      applicationName: "Plankton Eye",
+      applicationVersion: "1.0",
+      applicationLegalese: "By Jordan Janakievski",
+      children: <Widget>[
+        Text(
+            "Created for Bellarmine Preparatory School's Marine Chemistry program"),
+      ],
+    );
+  }
+}
---
+++ lib/realtime/live_camera.dart
@@ -0,0 +1,67 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:object_detection/realtime/bounding_box.dart';
+import 'package:object_detection/realtime/camera.dart';
+import 'dart:math' as math;
+import 'package:tflite/tflite.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LiveFeed extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  LiveFeed(this.cameras);
+  @override
+  _LiveFeedState createState() => _LiveFeedState();
+}
+
+class _LiveFeedState extends State<LiveFeed> {
+  List<dynamic> _recognitions;
+  int _imageHeight = 0;
+  int _imageWidth = 0;
+  initCameras() async {}
+  loadTfModel() async {
+    await Tflite.loadModel(
+      //This dictates the model that is being used.
+      //Changing the path to a different tflite file will change the model
+      model: "assets/models/detect.tflite",
+      labels: "assets/models/plankton_labels.txt",
+    );
+  }
+  setRecognitions(recognitions, imageHeight, imageWidth) {
+    setState(() {
+      _recognitions = recognitions;
+      _imageHeight = imageHeight;
+      _imageWidth = imageWidth;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadTfModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Plankton Eye",
+            style: GoogleFonts.openSans(
+              textStyle: TextStyle(color: Colors.white, fontSize: 25),
+            )),
+      ),
+      body: Stack(
+        children: <Widget>[
+          CameraFeed(widget.cameras, setRecognitions),
+          BoundingBox(
+            _recognitions == null ? [] : _recognitions,
+            math.max(_imageHeight, _imageWidth),
+            math.min(_imageHeight, _imageWidth),
+            screen.height,
+            screen.width,
+          ),
+        ],
+      ),
+    );
+  }
+}
